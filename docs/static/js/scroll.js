cd40e2de4c6dec58c3968a7babc37795650c9267
Matthew McConnell <matthewmcconnell04@gmail.com>
2020-09-18 16:38:13 +0100

    Deleted all the unused doc folders stuff Also changed the contacts to the updated 2020 committee.

--- docs/static/js/scroll.js
+++
@@ -1,5 +0,0 @@
-$("#linker").click(function(){ goToByScroll(document.getElementById("linker").href); return false; });
-
-function goToByScroll(id){
-    $('html,body').animate({scrollTop: $("#"+id).offset().top},'slow');
-}
